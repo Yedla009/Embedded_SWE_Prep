fdd9f38c742c3afcb3b663b528bab5c680d7ed43
Yedla <14228270+Yedla009@users.noreply.github.com>
2025-02-07 01:38:00 -0500

    Update 4. Find position of the only bit that is set.c
    if there is only one bit, then it's a power of 2

--- Bit Manipulation/C/4. Find position of the only bit that is set.c
+++ Bit Manipulation/C/4. Find position of the only bit that is set.c
@@ -1,37 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// int powTwo(int n)
-// {
-//     if(n > 0)
-//     {
-//         if((n & (n - 1)) == 0)
-//         {
-//             return true;
-//         }
-//         else{
-//             return false;
-//         }
-//     }
-//     return false;
-// }
-
-int posOfsetBit(int n)
-{
-    int i = 1;
-    int pos = 1;
-    // if(powTwo(n) == false)
-    // {
-    //     return -1;
-    // }
-    // else{
-        while((n & i) == 0)
-        {
-            i = i << 1;
-            pos += 1;
-        }
-    // }
-    return pos;
+int posOfsetBit(int n){
+    if (!n) return 1;
+    return log2(n)+1;
 }
 
 int main()
